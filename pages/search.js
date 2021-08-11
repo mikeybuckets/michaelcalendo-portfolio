569c9b65d8067f14de25963a4d355f1593a083d9
mikeybuckets <mikeycalendo20@gmail.com>
2021-08-11 13:46:13 -0700

    search component start

---
+++ pages/search.js
@@ -0,0 +1,22 @@
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
+
+function Search() {
+    return (
+        <div>
+            <Header />
+
+            <main className="flex">
+                <section>
+                    <p className="text-xs">300 Stays for 5 number of guests</p>
+
+                </section>
+            </main>
+
+            <Footer />
+        </div>
+    );
+}
+
+export default Search;
