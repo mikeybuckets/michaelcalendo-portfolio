3922c22bc71cfbb5dbbffde11ea695f1f514db3a
mikeybuckets <mikeycalendo20@gmail.com>
2021-08-11 23:55:45 -0700

    Wednesday ond of night

--- pages/search.js
+++ pages/search.js
@@ -1,17 +1,30 @@
+import { useRouter } from "next/dist/client/router";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-
+import { format } from "date-fns";
 
 function Search() {
+    const router = useRouter();
+
+    const { location, startDate, endDate, noOfGuests } = router.query;
+
+    const formattedStartDate = format(new Date(startDate), "dd MMMM yy");
+    const formattedEndDate = format(new Date(endDate), "dd MMMM yy");
+    const range = `${formattedStartDate} - ${formattedEndDate}`;
+
     return (
         <div>
-            <Header />
+            <Header placeholder={`${location} | ${range} | ${noOfGuests}
+            guests`} />
 
             <main className="flex">
-                <section>
-                    <p className="text-xs">300+ Stays for 5 number of guests</p>
+                <section className="flex-grow pt-14 px-6">
+                    <p className="text-xs">300+ Stays - {range} - 
+                    for {noOfGuests} guests</p>
 
-                    <h1 className="text-3xl font-semibold-lg mt-2 mb-6">Stays in Mars</h1>
+                    <h1 className="text-3xl font-semibold-lg mt-2 mb-6">
+                        Stays in {location}
+                    </h1>
 
                     <div className="hidden lg:inline-flex mb-5 space-x-3
                     text-gray-800 whitespace-nowrap">
@@ -21,7 +34,7 @@ function Search() {
                         <p className="button">Rooms and Beds</p>
                         <p className="button">More filters</p>
                     </div>
-                </section>
+                </section> 
             </main>
 
             <Footer />
