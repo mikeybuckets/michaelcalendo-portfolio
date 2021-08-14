faffe42c2a09f52b5091169e61ea52b379786169
mikeybuckets <mikeycalendo20@gmail.com>
2021-08-13 17:30:24 -0700

    5:30PM

--- components/Map.js
+++ components/Map.js
@@ -1,12 +1,12 @@
 import ReactMapGL from "react-map-gl";
 
 function Map() {
-    return <ReactMapGL
-        mapStyle="mapbox://styles/divops/cksaylyls14bs17mwka35lf9a"
-    >
-        
-    </ReactMapGL>;
-
+    return (
+        <ReactMapGL
+            mapStyle="mapbox://styles/divops/cksaylyls14bs17mwka35lf9a"
+            mapboxApiAccessToken={process.env.mapbox_key}
+        ></ReactMapGL>
+    );
 }
 
 export default Map;
